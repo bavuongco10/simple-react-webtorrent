d6caa1aa22fb241cc7842268db890630bb17276a
Bui Thanh Ba Vuong <bavuongco10@gmail.com>
2019-05-29 05:38:33 +0700

    Updates

---
+++ precache-manifest.a29274cb7642d5a4b338de9d4e8c0b6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e2a1eda08f220afa8cc864b00bc01f5",
+    "url": "/simple-react-webtorrent/index.html"
+  },
+  {
+    "revision": "780516a9829b7b48f2ef",
+    "url": "/simple-react-webtorrent/static/css/main.94ad8322.chunk.css"
+  },
+  {
+    "revision": "a3967750b87473670bf1",
+    "url": "/simple-react-webtorrent/static/js/2.764600bf.chunk.js"
+  },
+  {
+    "revision": "780516a9829b7b48f2ef",
+    "url": "/simple-react-webtorrent/static/js/main.b7838ec2.chunk.js"
+  },
+  {
+    "revision": "31e3648208246e7a59eb",
+    "url": "/simple-react-webtorrent/static/js/runtime~main.ff42af93.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/simple-react-webtorrent/static/media/logo.5d5d9eef.svg"
+  }
+]);
